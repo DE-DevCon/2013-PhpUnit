27456d3cc3fa401e5a054c6b30cc8ad14b883650
Chris Ryan <christopher.ryan@dominionenterprises.com>
2013-12-11 16:46:10 -0500

    Specify @covers annotation so we are explicit about what code we are testing.

--- tests/DoMathTest.php
+++ tests/DoMathTest.php
@@ -1,13 +1,21 @@
 <?php
 /**
  * Test the math class
+ *
+ * @covers DoMath
  */
 final class DoMathTest extends PHPUnit_Framework_TestCase {
+    /**
+     * @covers DoMath::add
+     */
     public function testAddIntegers() {
         $obj = new DoMath();
         $this->assertEquals(3, $obj->add(1, 2));
     }
 
+    /**
+     * @covers DoMath::add
+     */
     public function testAddFloats() {
         $obj = new DoMath();
         $this->assertEquals(1.3, $obj->add(0.5, 0.8));
