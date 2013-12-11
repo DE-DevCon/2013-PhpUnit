724a4f85c7333029a949ee739c188639844e1489
Chris Ryan <christopher.ryan@dominionenterprises.com>
2013-12-11 16:37:34 -0500

    Adding more descriptive tests. Try running phpunit with --testdox to see a nice check list based on the test names.

--- tests/DoMathTest.php
+++ tests/DoMathTest.php
@@ -3,8 +3,13 @@
  * Test the math class
  */
 final class DoMathTest extends PHPUnit_Framework_TestCase {
-    public function testAdd() {
+    public function testAddIntegers() {
         $obj = new DoMath();
         $this->assertEquals(3, $obj->add(1, 2));
+    }
+
+    public function testAddFloats() {
+        $obj = new DoMath();
+        $this->assertEquals(1.3, $obj->add(0.5, 0.8));
     }
 }
